48f6cf15b82302eac46525891e929f25566f7c7a
Jacob Brewer <Jacob.brewer@bthree.uk>
2024-02-05 12:07:43 +0000

    fixing template

--- pkg/dumpster/template.go
+++ pkg/dumpster/template.go
@@ -7,18 +7,15 @@ DROP DATABASE IF EXISTS {{ .Database }};
 
 {{range .Tables}}
 -- Table structure for table {{ .Name }}
-DROP TABLE IF EXISTS {{ .Name }};
 {{ .SQL }};
 
 -- Data dump for table {{ .Name }}
 LOCK TABLES {{ .Name }} WRITE;
-/*!40000 ALTER TABLE {{ .Name }} DISABLE KEYS */;
 {{ if .Values }}
 INSERT INTO {{ .Name }} VALUES {{ .Values }};
 {{ end }}
-/*!40000 ALTER TABLE {{ .Name }} ENABLE KEYS */;
 UNLOCK TABLES;
 {{ end }}
 
--- Dump completed on {{ .CompleteTime }}
+-- Dump completed at {{ .CompleteTime }}
 `
